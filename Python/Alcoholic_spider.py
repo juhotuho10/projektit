fd20a5c2b6482e54b3e1b741ffc1734f66ee5b9e
= <=>
2022-03-11 20:34:48 +0200

    spider parannuksia

--- Python/Alcoholic_spider.py
+++ Python/Alcoholic_spider.py
@@ -22,13 +22,16 @@ product_count = int("".join(product_count))
 print("tuotemäärä: ", product_count)
 
 # sivumäärä saadaan tuotemäärästä
-pages = floor(product_count / 12)
+if product_count % 12 == 0:
+    pages = floor(product_count / 12)-1
+else:
+    pages = floor(product_count / 12)
 
 print("sivumäärä: ", pages)
 
 # lataa Alkon nettisivut kaikki sivumäärät mukaanlukien
 
-url = "https://www.alko.fi/tuotteet/tuotelistaus?SearchTerm=*&PageSize=12&SortingAttribute=&PageNumber=" + str(pages) + "&SearchParameter=%26%40QueryTerm%3D*%26ContextCategoryUUID%3D6Q7AqG5uhTIAAAFVOmkI2BYA%26OnlineFlag%3D1"
+url = "https://www.alko.fi/tuotteet/tuotelistaus?SearchTerm=*&PageSize=12&SortingAttribute=&PageNumber=" + str(pages-1) + "&SearchParameter=%26%40QueryTerm%3D*%26ContextCategoryUUID%3D6Q7AqG5uhTIAAAFVOmkI2BYA%26OnlineFlag%3D1"
 
 print(url)
 
