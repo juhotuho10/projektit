264bf321113c9ee22c16e8d79b799c61eb50ffe6
Juho Naatula <juhotuho10@gmail.com>
2023-12-29 17:25:45 +0200

    converted to a pytorch model

--- Python/ML-2048/pytorch_training.py
+++ Python/ML-2048/pytorch_training.py
@@ -1,8 +1,9 @@
 from game2048 import Game2048
 
 import numpy as np
-from keras.models import Model
-from keras.layers import Input, Dense, Conv2D, Flatten, Concatenate
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
 
 game = Game2048()
 
@@ -10,46 +11,74 @@ print(game.get_board())
 game.move_board("left")
 print(game.get_board())
 
-def create_model():
-    # model that takes in both the game grid into the CNN layers as well as other data about the game
-    # the CNN layer and the dense layer concatinate halfway down the model
-    # this is done because the other features arent spacially related to the game grid so they arent fed through the CNN
-    grid_input = Input(shape=(4, 4, 1)) 
-    cnn = Conv2D(filters=64, kernel_size=(2, 2), activation='relu')(grid_input)
-    cnn = Flatten()(cnn)
+class DualInputModel(nn.Module):
+    def __init__(self):
+        super(DualInputModel, self).__init__()
 
-    features_input = Input(shape=(5,))
-    dense = Dense(units=64, activation='relu')(features_input)
+        # CNN branch for the game grid
+        self.conv1 = nn.Conv2d(in_channels=1, out_channels=64, kernel_size=(2, 2))
+        self.flatten = nn.Flatten()
 
-    combined = Concatenate()([cnn, dense])
+        # Dense branch for the features
+        self.fc_features = nn.Linear(in_features=5, out_features=64)
 
-    combined = Dense(units=128, activation='relu')(combined)
-    combined = Dense(units=64, activation='relu')(combined)
-    combined = Dense(units=32, activation='relu')(combined)
+        # The size of the flattened CNN output needs to be calculated correctly
+        # Assuming the input size is (1, 4, 4), after Conv2d(2x2) and flatten, it becomes 64 * (3 * 3)
+        cnn_output_size = 64 * 3 * 3  # Adjust this based on your actual CNN output
 
-    output = Dense(units=1, activation='sigmoid')(combined)
+        # Combined layers
+        self.fc1 = nn.Linear(in_features=cnn_output_size + 64, out_features=128)
+        self.fc2 = nn.Linear(in_features=128, out_features=64)
+        self.fc3 = nn.Linear(in_features=64, out_features=32)
+        self.output = nn.Linear(in_features=32, out_features=1)
 
-    model = Model(inputs=[grid_input, features_input], outputs=output)
+    def forward(self, grid_input, features_input):
+        # CNN branch
+        x1 = F.relu(self.conv1(grid_input))
+        x1 = self.flatten(x1)
 
-    return model
+        # Dense branch
+        x2 = F.relu(self.fc_features(features_input))
+
+        # Combine branches
+        combined = torch.cat((x1, x2), dim=1)
+
+        # Additional dense layers
+        combined = F.relu(self.fc1(combined))
+        combined = F.relu(self.fc2(combined))
+        combined = F.relu(self.fc3(combined))
+        output = torch.sigmoid(self.output(combined))
+
+        return output
+
 
 def get_data():
     board = np.array(game.get_board())
     
-
+    # taking a log2 of the board, so 0 = 0, 2 = 1, 4 = 2, 8 = 3 etc...
+    log_board = np.log2(board, where=(board!=0)).astype(int).astype(np.float32)
+    
     empty_count = sum(board.flatten() == 0)
+
     max_num_count = np.sum(board == np.max(board))
 
     mergeable_rows, mergeable_cols = count_mergeable_tiles()
     max_mergeable = max([mergeable_rows, mergeable_cols])
+
     valid_moves = get_valid_moves(mergeable_rows, mergeable_cols)
     valid_move_count = len(valid_moves)
 
     max_value_score = get_max_value_score()
 
-    other_data = np.array([empty_count, max_num_count, max_mergeable, valid_move_count, max_value_score])
+    # Ensure the grid_input has the correct shape
+    log_board = np.expand_dims(log_board, axis=0) 
+    log_board = np.expand_dims(log_board, axis=0) 
 
-    return board, other_data
+
+    other_data = np.array([empty_count, max_num_count, max_mergeable, valid_move_count, max_value_score], dtype=np.float32)
+    other_data = np.expand_dims(other_data, axis=0) 
+
+    return log_board, other_data
 
 def count_mergeable_tiles():
     board = game.get_board()
@@ -78,13 +107,13 @@ def get_valid_moves(mergeable_rows, mergeable_cols):
     board = np.array(game.get_board())
     valid_directions = []
 
-    if mergeable_rows != 0 and mergeable_cols !=0:
-        return ["up", "left", "down", "right"]
-
     if mergeable_rows != 0:
         valid_directions.extend(["left", "right"])
     if mergeable_cols != 0:
         valid_directions.extend(["up", "down"])
+
+    if len(set(valid_directions)) == 4:
+        return ["up", "left", "down", "right"]
 
     sub_array = board[1:3, 1:3]
     if np.any(sub_array == 0):
@@ -115,6 +144,7 @@ def get_max_value_score():
     max_value = np.max(board)
 
     # checks if max value is in a corner
+    # [(x coords), (y coords)]
     corner_coordinates = [(0,0,3,3), (0,3,0,3)]
     corner_values = board[tuple(zip(corner_coordinates))]
     if np.any(corner_values == max_value):
@@ -127,6 +157,15 @@ def get_max_value_score():
     else:
         return 0
 
+# Example model predictions for each sample
+model = DualInputModel()
+model.eval()  # Set the model to evaluation mode
 
-get_data()
-print(count_mergeable_tiles())
+with torch.no_grad():  # Disable gradient computation
+    grid_input, features_input = get_data()
+
+    grid_input = torch.from_numpy(grid_input)
+    features_input = torch.from_numpy(features_input)
+    
+    predicted_value = model(grid_input, features_input)
+    print(f"Predicted Value: {predicted_value.item()}")
