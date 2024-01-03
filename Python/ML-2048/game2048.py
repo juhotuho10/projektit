e4ffa6568cf81abbbe043457d35bb0c069e04e4b
Juho Naatula <juhotuho10@gmail.com>
2024-01-03 15:57:47 +0200

    accelerated compute with numba

--- Python/ML-2048/game2048.py
+++ Python/ML-2048/game2048.py
@@ -1,5 +1,39 @@
 import numpy as np
 import random
+from numba import jit
+
+@jit(nopython=True)
+def compress(grid: np.ndarray) -> np.ndarray:
+
+    new_grid = np.zeros_like(grid)
+    for i in range(4):
+        non_zero_elements = grid[i][grid[i] != 0]
+        new_grid[i, :len(non_zero_elements)] = non_zero_elements
+    return new_grid
+
+@jit(nopython=True)
+def merge(grid:np.ndarray) -> np.ndarray:
+    for i in range(4):
+        for j in range(3):
+            if grid[i, j] == grid[i, j + 1] and grid[i, j] != 0:
+                grid[i, j] *= 2
+                grid[i, j + 1] = 0
+    return grid
+
+'''@jit(nopython=True)
+def reverse(grid: np.ndarray) -> np.ndarray:
+    return np.flip(grid, axis=1)'''
+
+@jit(nopython=True)
+def reverse(grid: np.ndarray) -> np.ndarray:
+    reversed_grid = np.zeros_like(grid)
+    for i in range(grid.shape[0]):
+        reversed_grid[i] = grid[i][::-1]
+    return reversed_grid
+
+@jit(nopython=True)
+def transpose(grid: np.ndarray) -> np.ndarray:
+    return np.transpose(grid)
 
 class Game2048:
     def __init__(self, size=4):
@@ -11,7 +45,7 @@ class Game2048:
         self.board = np.zeros((self.size, self.size), dtype=int)
         self.add_new_tile()
         self.add_new_tile()
-    
+
     def add_new_tile(self):
         zero_coords = np.where(self.board == 0)
         zero_coord_tuples = list(zip(zero_coords[0], zero_coords[1]))
@@ -19,58 +53,28 @@ class Game2048:
             i, j = random.choice(zero_coord_tuples)
             self.board[i, j] = random.choice([2, 4])
 
-    def set_new_tile(self, tile):
-        if tile:
-            i, j = tile[0]
-            number = tile[1]
-            self.board[i, j] = number
-
-    
-    def compress(self, grid: np.ndarray):
-
-        new_grid = np.zeros_like(grid)
-        for i in range(4):
-            non_zero_elements = grid[i][grid[i] != 0]
-            new_grid[i, :len(non_zero_elements)] = non_zero_elements
-        return new_grid
-
-    def merge(self, grid:np.ndarray):
-        for i in range(4):
-            for j in range(3):
-                if grid[i, j] == grid[i, j + 1] and grid[i, j] != 0:
-                    grid[i, j] *= 2
-                    grid[i, j + 1] = 0
-        return grid
-
-    def reverse(self, grid: np.ndarray):
-        return np.flip(grid, axis=1)
-
-    def transpose(self, grid: np.ndarray):
-        return np.transpose(grid)
-    
     def move(self, direction):
 
         match direction:
             case "left":
-                self.board = self.compress(self.board)
-                self.board = self.merge(self.board)
-                self.board = self.compress(self.board)
+                self.board = compress(self.board)
+                self.board = merge(self.board)
+                self.board = compress(self.board)
 
             case "right":
-                self.board = self.reverse(self.board)
+                self.board = reverse(self.board)
                 self.move("left")
-                self.board = self.reverse(self.board)
+                self.board = reverse(self.board)
 
             case "up":
-                self.board = self.transpose(self.board)
+                self.board = transpose(self.board)
                 self.move("left")
-                self.board = self.transpose(self.board)
+                self.board = transpose(self.board)
 
             case "down":
-                self.board = self.transpose(self.board)
+                self.board = transpose(self.board)
                 self.move("right")
-                self.board = self.transpose(self.board)
-
+                self.board = transpose(self.board)
 
     def move_board(self, direction):
         assert direction in ["left", "right", "up", "down"]
@@ -95,6 +99,7 @@ class Game2048:
 
         return is_valid
     
+
     def board_from_move(self, direction):
         temp_game = Game2048()
         temp_game.board = np.copy(self.get_board())
@@ -102,6 +107,5 @@ class Game2048:
 
         return temp_game.get_board()
     
-
     def get_board(self):
         return self.board
